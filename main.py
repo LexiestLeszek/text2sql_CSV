d30fe09bdbd7eee156369480951ff955b12dad39
Leszek Mielnikow <leszekmel@gmail.com>
2024-03-12 20:28:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,74 +1,43 @@
-import pandas as pd
-import sqlite3
-from sqlite3 import Error
 from llmware.agents import LLMfx
+import pandas as pd
 
-###########################################################
-# RUN THIS THING ONCE TO DOWNLOAD LOCAL LLMs FROM LLM_WARE:
+#from llmware.models import ModelCatalog
+#ModelCatalog().get_llm_toolkit(["sql"])
 
-from llmware.models import ModelCatalog
+from llmware.agents import LLMfx
+import pandas as pd
 
-ModelCatalog().get_llm_toolkit(["sql"])
-
-ModelCatalog().tool_test_run("slim-sql-tool")
-
-###########################################################
-###########################################################
-
-def main(csv_file_path):
+def main():
+    
+    csv_file_path = "StockRatings.csv"
+    
+    query = "What are the companies with the best overall rating?"
     
     df = pd.read_csv(csv_file_path)
-    
-    conn = sqlite3.connect(":MEMORY:")
-    
+
+    sample_table_schema = """
+            CREATE TABLE 
+            customer_info (
+                customer_name text, 
+                account_number integer, 
+                annual_spend integer
+                )"""
+                
+    column_names = df.columns.tolist()
+    data_type = ""
+    column_names_string = ', '.join([f'{name} {data_type}' for name in column_names])
     table_name = csv_file_path.split('/')[-1].split('.')[0]
-    df.to_sql(table_name, conn, if_exists='replace', index=False)
-    
-    cursor = conn.cursor()
-    cursor.execute(f"PRAGMA table_info({table_name})")
-    schema = cursor.fetchall()
-    
-    schema_string = ",".join(f'"{column[1]}"' for column in schema)
-    
-    db_schema = f"CREATE TABLE {table_name} ({schema_string})"
-    
-    print(db_schema)
-    
-    query = "Who has the best overall rating?"
+    schema_from_csv = f"CREATE TABLE {table_name} ({column_names_string})"
 
     agent = LLMfx(verbose=False)
-    response_json = agent.sql(query, db_schema)
+    response_json = agent.sql(query, schema_from_csv)
     
-    response = response_json["llm_response"]
-    
-    print(response)
-    
-    words1 = response.split()
-    words2 = db_schema.split()
-    
-    for i in range(len(words1)):
+    response = response_json['llm_response']
 
-        if words1[i] in words2 and not (words1[i].startswith('"') and words1[i].endswith('"')):
-    
-            words1[i] = f'"{words1[i]}"'
-    
-    modified_string = ' '.join(words1)
-    
-    try:
-        cursor.execute(modified_string)
-        rows = cursor.fetchall()
-        print(rows)
-        return rows
-    except Error as e:
-        print(e)
-        rows = None
-    finally:
-        if conn:
-            conn.close()
+    print(response)
+
+    return response
 
 if __name__ == "__main__":
     
-    file_path = "./StockRatings.csv"
-    
-    main(file_path)
-    
+    main()
